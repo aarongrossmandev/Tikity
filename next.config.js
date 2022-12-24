88b38c2111be3e58d5ce9015d2233276b72044a8
Aaron <aarongrossman.dev@gmail.com>
2022-12-24 12:18:35 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["img.icons8.com",
+    "lh3.googleusercontent.com",
+   ],
+  }
 }
 
 module.exports = nextConfig
